b7016f1f8bbbb51ee9f437410bd8d1aa7a7ff61c
Daniel Kaminski <daniel.kaminski@hotmail.com>
2019-10-12 19:01:02 +0200

    Fix repository error

--- backend/src/main/java/pl/waw/goodenough/iautostop/model/dto/CoordinatesDto.java
+++ backend/src/main/java/pl/waw/goodenough/iautostop/model/dto/CoordinatesDto.java
@@ -5,20 +5,10 @@ import lombok.Data;
 
 @Data
 @Builder
-public class CoordinatesDto {
+public class CoordinatesDTO {
 
     private Double latitude;
     private Double longitude;
-
-    public static CoordinatesDto parseString(String stringCoordinates) {
-
-        String[] coordinates = stringCoordinates.split(",");
-
-        return CoordinatesDto.builder()
-                .latitude(Double.parseDouble(coordinates[0]))
-                .longitude(Double.parseDouble(coordinates[1]))
-                .build();
-    }
 
     public String toString() {
       return longitude.toString() + "," + latitude.toString();
