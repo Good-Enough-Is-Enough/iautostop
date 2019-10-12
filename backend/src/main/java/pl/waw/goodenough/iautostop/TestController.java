f0b818c4eb5fa8f52cd694b0b4d1927598576198
Przemek Zych <zychster@gmail.com>
2019-10-12 17:13:22 +0200

    Driver can be logged in

---
+++ backend/src/main/java/pl/waw/goodenough/iautostop/TestController.java
@@ -0,0 +1,21 @@
+package pl.waw.goodenough.iautostop;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.waw.goodenough.iautostop.repository.RouteRepository;
+
+import java.util.List;
+
+@RestController
+public class TestController {
+
+    @Autowired
+    RouteRepository routeRepository;
+
+    @GetMapping("/test")
+    public List<String> dupa(String from, String to) {
+        return routeRepository.getRouteStreetNames(from, to);
+    }
+
+}
