8c37639778919e516a36aad9af2b0d1b1b87cd47
Przemek Zych <zychster@gmail.com>
2019-10-12 16:38:38 +0200

    fix

--- backend/src/main/java/pl/waw/goodenough/iautostop/repository/HereRouteRepository.java
+++ backend/src/main/java/pl/waw/goodenough/iautostop/repository/HereRouteRepository.java
@@ -4,14 +4,18 @@ import com.jayway.jsonpath.JsonPath;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class HereRouteRepository implements RouteRepository {
 
     private static final String APP_ID = "i8w3y91fhZ5nDPijP7XL";
     private static final String APP_CODE = "v6YKXNdMEl2eGoTGHyToSQ";
     private static final String MODE = "fastest;car;traffic:disabled";
-    public static final String URL = "https://route.api.here.com/routing/7.2/calculateroute.json";
+    private static final String URL = "https://route.api.here.com/routing/7.2/calculateroute.json";
+    private static final String MANOUVER_ATTRIBUTES = "roadName";
 
     private RestTemplate restTemplate;
 
@@ -22,10 +26,19 @@ public class HereRouteRepository implements RouteRepository {
     public List<String> getRouteStreetNames(String travelFrom, String travelTo) {
 
         String url = URL + "?" + "app_id=" + APP_ID + "&app_code=" + APP_CODE + "&waypoint0=" + travelFrom + "&waypoint1=" +
-                travelTo + "&mode=" + MODE;
+                travelTo + "&mode=" + MODE +"&maneuverAttributes=" + MANOUVER_ATTRIBUTES;
         ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-        String jsonPath = "$['response']['route'][0]['waypoint'][*]['mappedRoadName']";
+        String jsonPath = "$['response']['route'][0]['leg'][0]['maneuver'][*]['roadName']";
         String route = response.getBody();
-        return JsonPath.parse(route).read(jsonPath);
+        List<String> streets = JsonPath
+                .parse(route)
+                .read(jsonPath);
+
+        Set<String> noDuplicateStreets= new HashSet<>(streets);
+
+        return noDuplicateStreets
+                .stream()
+                .filter(s -> !s.isEmpty())
+                .collect(Collectors.toList());
     }
 }
