22510de663664fbafddc0e460daf403e0c3c4a44
Daniel Kaminski <daniel.kaminski@hotmail.com>
2019-10-12 19:02:16 +0200

    Fix repository error

--- backend/src/main/java/pl/waw/goodenough/iautostop/model/dto/CoordinatesDto.java
+++ backend/src/main/java/pl/waw/goodenough/iautostop/model/dto/CoordinatesDto.java
@@ -10,6 +10,16 @@ public class CoordinatesDto {
     private Double latitude;
     private Double longitude;
 
+    public static CoordinatesDto parseString(String stringCoordinates) {
+
+        String[] coordinates = stringCoordinates.split(",");
+
+        return CoordinatesDto.builder()
+                .latitude(Double.parseDouble(coordinates[0]))
+                .longitude(Double.parseDouble(coordinates[1]))
+                .build();
+    }
+
     public String toString() {
       return longitude.toString() + "," + latitude.toString();
     }
