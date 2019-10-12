ecf2fe6ebdf13d6ea7b1df4339f306d1265eb55b
pabloscript <pe.kozicki@gmail.com>
2019-10-12 21:44:42 +0200

    FE - update styles, add validation fo let's go button, format code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,15 +3,16 @@
     <v-app id="inspire">
       <div>
         <v-app-bar color="red accent-4" dark>
-          <img alt="Vue logo" src="./assets/logo.png" height="50px" />
+          <img alt="Vue logo" src="./assets/logo.png" height="50px"/>
           <v-toolbar-title>iAutostop</v-toolbar-title>
         </v-app-bar>
 
         <router-view />
 
-        <v-footer>
-          <div class="flex-grow-1"></div>
-          <div>&copy; {{ new Date().getFullYear() }}</div>
+        <v-footer absolute class="text-center">
+          <v-col class="text-center" cols="12">
+            &copy; {{ new Date().getFullYear() }} — <strong>GoodEnough</strong>
+          </v-col>
         </v-footer>
       </div>
     </v-app>
