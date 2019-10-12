49c28d35d36ca18469269230e89cf59600520851
Daniel Kaminski <kaminski.daniel@hotmail.com>
2019-10-12 19:08:19 +0200

    Fix repository error

---
+++ backend/src/main/java/pl/waw/goodenough/iautostop/model/dto/CoordinatesDto.java
@@ -0,0 +1,26 @@
+package pl.waw.goodenough.iautostop.model.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CoordinatesDto {
+
+    private Double latitude;
+    private Double longitude;
+
+    public static CoordinatesDto parseString(String stringCoordinates) {
+
+        String[] coordinates = stringCoordinates.split(",");
+
+        return CoordinatesDto.builder()
+                .latitude(Double.parseDouble(coordinates[0]))
+                .longitude(Double.parseDouble(coordinates[1]))
+                .build();
+    }
+
+    public String toString() {
+        return longitude.toString() + "," + latitude.toString();
+    }
+}
