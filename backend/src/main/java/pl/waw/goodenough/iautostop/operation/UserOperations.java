83c41228458f9ab236a44ca98484338686b85bc8
Damianke <damian.oracki@gmail.com>
2019-10-12 21:46:40 +0200

    fix of fix of fix

--- backend/src/main/java/pl/waw/goodenough/iautostop/operation/UserOperations.java
+++ backend/src/main/java/pl/waw/goodenough/iautostop/operation/UserOperations.java
@@ -64,7 +64,7 @@ public class UserOperations {
             CoordinatesDto coordinatesDtoTo = routeService.getCoordinatesByLocationId(userLoggedInDto.getTravelTo());
             List<String> routeStreetNamesList = routeService.getRouteStreetNames(coordinatesDtoFrom, coordinatesDtoTo);
 
-            final String routeStreetNames = routeStreetNamesList.toString()
+            final String routeStreetNames = routeStreetNamesList.toString();
             appUserRoute.setTravelStreetList(routeStreetNames.substring(1, routeStreetNames.length() - 1));
         }
 
