77ca45ce3985d4a4e4de0357927f55fce54ef122
Arkadiusz Karapuda <arkadiusz.karapuda@gmail.com>
2019-10-12 21:05:53 +0200

    [FE] - Passengers List

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,3 +1,4 @@
 export const HERE_MAP_APP_ID = "i8w3y91fhZ5nDPijP7XL";
 export const HERE_MAP_APP_CODE = "v6YKXNdMEl2eGoTGHyToSQ";
 export const SET_USER_INFO = "/users";
+export const AVAILABLE_PASSENGERS = "/available-passengers";
