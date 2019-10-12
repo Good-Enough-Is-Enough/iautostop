5355b23fa0833ce45a0eb0d0cc83e2475149f42d
Arkadiusz Karapuda <arkadiusz.karapuda@gmail.com>
2019-10-12 20:23:31 +0200

    [FE] - Fix req

--- frontend/src/views/SetTripView.vue
+++ frontend/src/views/SetTripView.vue
@@ -1,14 +1,16 @@
 <template>
   <v-content>
     <route-selector
+      :disabled="isLoading"
       :direction="'From'"
       @route-select="onRouteSelect($event, 'from')"
     />
     <route-selector
+      :disabled="isLoading"
       :direction="'To'"
       @route-select="onRouteSelect($event, 'to')"
     />
-    <v-btn @click="setUserInfo">Let's go!</v-btn>
+    <v-btn :loading="isLoading" @click="setUserInfo">Let's go!</v-btn>
   </v-content>
 </template>
 
@@ -25,6 +27,7 @@ export default {
   },
   data() {
     return {
+      isLoading: false,
       user: {},
       travelFrom: '',
       travelTo: ''
@@ -44,7 +47,19 @@ export default {
         travelTo: this.travelTo
       };
 
-      fetch(SET_USER_INFO, {method: "POST", body: JSON.stringify(this.user)});
+      this.isLoading = true;
+
+      fetch(SET_USER_INFO, {
+        method: "POST",
+        body: JSON.stringify(this.user),
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+      .catch(err => {
+        console.error(err)
+      })
+      .finally(() => (this.isLoading = false));
     }
   }
 };
