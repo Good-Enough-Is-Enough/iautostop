2f3c88390e55e44b55086033000f8d2ce7c4576d
Arkadiusz Karapuda <arkadiusz.karapuda@gmail.com>
2019-10-13 14:27:52 +0200

    [FE] - Display streets

--- frontend/src/views/DriverPanel.vue
+++ frontend/src/views/DriverPanel.vue
@@ -1,16 +1,36 @@
 <template>
   <v-content>
-    <v-container class="fill-height" fluid>
+    <v-container fluid>
       <v-row no-gutters>
         <v-col cols="12">
           <div class="text-center mb-2">
             <v-btn text color="deep-purple" @click="showMap = !showMap">
-              Pokaż mapę trasy
+              {{ showMap ? "Ukryj " : "Pokaż " }}mapę trasy
             </v-btn>
           </div>
         </v-col>
         <v-col cols="12">
           <route-map :from="from" :to="to" v-show="showMap" />
+        </v-col>
+        <v-col cols="12">
+          <div class="text-center mb-2">
+            <v-btn text color="deep-purple" @click="showRoute = !showRoute">
+              {{ showRoute ? "Ukryj " : "Pokaż " }}trasę
+            </v-btn>
+          </div>
+        </v-col>
+        <v-col cols="12" v-show="showRoute">
+          <p>Przebieg trasy</p>
+          <v-list>
+            <v-list-item v-for="(street, index) in streetNames" :key="index">
+              <v-list-item-icon>
+                <v-icon v-text="'mdi-map-marker-path'" />
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title v-text="street" />
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
         </v-col>
         <v-col cols="12">
           <passengers-list-view :id="id" />
@@ -34,9 +54,11 @@ export default {
   data() {
     return {
       showMap: false,
+      showRoute: false,
       isLoading: false,
       from: null,
-      to: null
+      to: null,
+      streetNames: null
     }
   },
   mounted() {
@@ -55,6 +77,7 @@ export default {
       .then((res) => {
         this.from = res.startCoords;
         this.to = res.endCoords;
+        this.streetNames = res.streetNames;
       })
       .catch((err) => {
         console.error(err);
