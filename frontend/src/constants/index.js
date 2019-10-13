3c1d06fe6c17c671606cf679cb439e0a2e29e960
Arkadiusz Karapuda <arkadiusz.karapuda@gmail.com>
2019-10-13 09:44:09 +0200

    [FE] - Notification

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,4 +1,5 @@
+export const MAIN_API_PATH = "/api/users";
 export const HERE_MAP_APP_ID = "i8w3y91fhZ5nDPijP7XL";
 export const HERE_MAP_APP_CODE = "v6YKXNdMEl2eGoTGHyToSQ";
-export const SET_USER_INFO = "/users";
+export const SET_USER_INFO = MAIN_API_PATH;
 export const AVAILABLE_PASSENGERS = "/available-passengers";
--- frontend/src/utils/notificationService.js
+++ frontend/src/utils/notificationService.js
@@ -1,17 +1,13 @@
-export const notifyMe = (text) => {
-    if (!("Notification" in window)) {
-        alert("This browser does not support system notifications");
-    }
-
-    else if (Notification.permission === "granted") {
+export const notifyMe = text => {
+  if (!("Notification" in window)) {
+    alert("This browser does not support system notifications");
+  } else if (Notification.permission === "granted") {
+    new Notification(text);
+  } else if (Notification.permission !== "denied") {
+    Notification.requestPermission(function(permission) {
+      if (permission === "granted") {
         new Notification(text);
-    }
-
-    else if (Notification.permission !== 'denied') {
-        Notification.requestPermission(function (permission) {
-            if (permission === "granted") {
-                new Notification(text);
-            }
-        });
-    }
+      }
+    });
+  }
 };
