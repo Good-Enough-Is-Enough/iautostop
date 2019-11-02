eb7f6eddaf8fab0268ec29be6a2eed0a01a41608
pabloscript <pe.kozicki@gmail.com>
2019-11-02 02:16:03 +0100

    refactor/divide-passenger-panel: index components

---
+++ frontend/src/components/index.js
@@ -0,0 +1,6 @@
+import LiftStatusMessage from "./LiftStatusMessage";
+import LocationSelector from "./LocationSelector";
+import PassengersList from "./PassengersList";
+import RouteMap from "./RouteMap";
+
+export { LiftStatusMessage, LocationSelector, PassengersList, RouteMap };
