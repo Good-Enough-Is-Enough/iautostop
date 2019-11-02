a3fad3b63a83732c920b87800e3bb0b8ea0eddb7
pabloscript <pe.kozicki@gmail.com>
2019-11-02 02:16:56 +0100

    refactor/divide-passenger-panel: add images constant, code formatting

---
+++ frontend/src/constants/images.js
@@ -0,0 +1,7 @@
+const IMG = {
+  PASSENGER_WAITING: `https://cdn.pixabay.com/photo/2019/08/20/15/32/cialis-4419052_960_720.jpg`,
+  PASSENGER_SUCCESS: `https://previews.123rf.com/images/tanyalmera/tanyalmera1606/tanyalmera160600005/58296044-hooray-word-cartoon-hand-lettering-positive-saying-for-cards-posters-and-social-media-content-happin.jpg`,
+  PASSENGER_ERROR: `https://www.elegantthemes.com/blog/wp-content/uploads/2016/03/500-internal-server-error-featured-image-1.png`
+};
+
+export default IMG;
--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,26 +1,39 @@
+import IMG from "./images";
 const MAIN_API_PATH = "/api/users";
-export const HERE_MAP_APP_ID = "i8w3y91fhZ5nDPijP7XL";
-export const HERE_MAP_APP_CODE = "v6YKXNdMEl2eGoTGHyToSQ";
-export const SET_USER_INFO = MAIN_API_PATH;
+const HERE_MAP_APP_ID = "i8w3y91fhZ5nDPijP7XL";
+const HERE_MAP_APP_CODE = "v6YKXNdMEl2eGoTGHyToSQ";
+const SET_USER_INFO = MAIN_API_PATH;
 const API_AVAILABLE_PASSENGERS = "/available-passengers";
 const API_ASSIGNED_DRIVER = "/assigned-driver";
 const API_STREET = "/streets";
 const API_CONNECT = "/connectedTo";
 
-export const getAvailablePassengersUrl = driverId =>
+const getAvailablePassengersUrl = driverId =>
   `${MAIN_API_PATH}/${driverId}${API_AVAILABLE_PASSENGERS}`;
 
-export const getRouteStreet = driverId =>
-  `${MAIN_API_PATH}/${driverId}${API_STREET}`;
+const getRouteStreet = driverId => `${MAIN_API_PATH}/${driverId}${API_STREET}`;
 
-export const getAvailableDriver = passengerId =>
+const getAvailableDriver = passengerId =>
   `${MAIN_API_PATH}/${passengerId}${API_ASSIGNED_DRIVER}`;
 
-export const connectTo = (passengerId, driverId) =>
+const connectTo = (passengerId, driverId) =>
   `${MAIN_API_PATH}/${passengerId}/connectTo/${driverId}`;
 
-export const getConnectedPassengers = driverId =>
+const getConnectedPassengers = driverId =>
   `${MAIN_API_PATH}${API_CONNECT}/${driverId}`;
 
-export const getEndTripUrl = driverId =>
+const getEndTripUrl = driverId =>
   `${MAIN_API_PATH}/driver/${driverId}/end-trip`;
+
+export {
+  IMG,
+  HERE_MAP_APP_ID,
+  HERE_MAP_APP_CODE,
+  SET_USER_INFO,
+  getAvailablePassengersUrl,
+  getRouteStreet,
+  getAvailableDriver,
+  connectTo,
+  getConnectedPassengers,
+  getEndTripUrl
+};
